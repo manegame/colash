c041d6fd2302f5de212f60344bae3a3a254a5e8d
manegame <manusnijhoff@gmail.com>
2017-03-21 11:29:20 +0100

    initial site online at long-show.surge.sh

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,52 @@
 
     global.$    = require("jquery");
 
+    require('fullpage.js');
+
+    var colash = {
+      1 : {
+        "url" : "img/colash-1.png",
+        "color" : "#407a15"
+      },
+      2 : {
+        "url" : "img/colash-2.jpg",
+        "color" : "#d8384b"
+      },
+      3 : {
+        "url" : "img/colash-3.jpg",
+        "color" : "#ffba00"
+      },
+      4 : {
+        "url" : "img/colash-4.jpg",
+        "color" : "#003e3a"
+      },
+      5 : {
+        "url" : "img/colash-5.jpg",
+        "color" : "#64462c"
+      }
+    }
+
+    function countProperties(obj) {
+        return Object.keys(obj).length;
+    }
+
+    function initSlides(obj) {
+
+
+      for ( var i = 1; i <= obj; i++ ){
+        if(i != obj) {
+          $('#fullpage').append('<div class="section" style="background-image:url('+colash[i].url+');background-color:'+colash[i].color+'"><div style="background-image:linear-gradient('+colash[i].color+', '+colash[(i+1)].color+')" class="gradient"></div> </div>');
+        } else {
+          $('#fullpage').append('<div class="section" style="background-image:url('+colash[i].url+');background-color:'+colash[i].color+'"></div>');
+        }
+      }
+      $('#fullpage').fullpage();
+
+    }
+
     $(function () {
+
+      initSlides(countProperties(colash));
 
 
     });
